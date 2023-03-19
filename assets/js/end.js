824c1092145841805b6de71fe01dd5e17d6ba33f
Juaisa99 <juarezisaac99gmail.com>
2023-03-18 20:18:15 -0700

    created JS for main questions and working on highscores Js, added style to questions, index2 starts the quiz game, endhtml is for the highscores sections.

---
+++ assets/js/end.js
@@ -0,0 +1,16 @@
+var username = document.getElementById('username');
+var saveScoreBtn = document.getElementById('saveScoreBtn');
+var finalScore = document.getElementById('finalScore');
+var mostRecentScore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentScore;
+
+
+username.addEventListener('keyup', () =>{
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    console.log('clicked the save button!');
+    e.preventDefault();
+};
